ec77163a842ef42848d31d292850e2e9703e842f
Levente Nagy <levente.nagy@flextronics.com>
2018-10-02 12:50:44 +0200

    electron 3.x bug, events not emitted from webview

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+window.addEventListener("keypress", (event) => {
+    console.log(event.which);
+    alert(event.which);
+});
